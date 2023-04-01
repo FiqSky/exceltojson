d1bc1ccea3e6bca89e10a38fd85f62ed89678c8a
fiqsky <fiqihfm05@gmail.com>
2023-04-02 01:17:05 +0700

    untuk data penyakit

--- excel_to_json.py
+++ excel_to_json.py
@@ -2,7 +2,7 @@ import pandas as pd
 import json
 
 # Ganti 'file_excel.xlsx' dengan nama file Excel Anda
-file_excel = 'datagejaladanbotot.xlsx'
+file_excel = 'file_excel.xlsx'
 
 # Ganti 'Sheet1' dengan nama sheet yang berisi data Anda dalam file Excel
 sheet_name = 'Sheet1'
@@ -11,10 +11,11 @@ sheet_name = 'Sheet1'
 data = pd.read_excel(file_excel, sheet_name=sheet_name, engine='openpyxl')
 
 # Mengonversi data ke dalam format JSON
+data['daftar_gejala'] = data['daftar_gejala'].apply(lambda x: x.split(', '))
 result = data.to_dict(orient='records')
 
 # Menyimpan hasil dalam file JSON
-with open('hasil.json', 'w') as file:
+with open('hasil_penyakit.json', 'w') as file:
     json.dump(result, file, ensure_ascii=False, indent=4)
 
 print('Konversi Excel ke JSON berhasil!')
